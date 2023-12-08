4a6968eae4ebcf7c26fc44098e3e9cb90e3a0167
Grzegorz Biełokozowicz <grzegorz.bielokozowicz@student.wat.edu.pl>
2023-12-08 08:36:02 +0100

    Init

---
+++ Projekt/Projekt/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Projekt
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+          
+
+        }
+
+        public void InitializeAvaiblePizzas()
+        {
+            avaiblePizzas.Items.Add(new Pizza("Margherita", new List<string> { "Sos pomidorowy", "Ser", "Bazylia" },29.99,33.99,37.99));
+        }
+    }
+}
---
+++ Projekt/Projekt/Pizza.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Projekt
+{
+    internal class Pizza
+    {
+        private string name;
+        private List<string> ingredients;
+        private double priceSmall;
+        private double priceMedium;
+        private double priceBig;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public List<string> Ingredients
+        {
+            get { return ingredients; }
+            set { ingredients = value; }
+        }
+
+      
+        public double PriceSmall
+        {
+            get { return priceSmall; }
+            set { priceSmall = value; }
+        }
+
+        public double PriceMedium
+        {
+            get { return priceMedium; }
+            set { priceMedium = value; }
+        }
+
+        public double PriceBig
+        {
+            get { return priceBig; }
+            set { priceBig = value; }
+        }
+
+        public Pizza(string name, List<string> ingredients, double priceSmall, double priceMedium, double priceBig)
+        {
+            this.Name = name;
+            this.Ingredients = ingredients;
+            this.PriceSmall = priceSmall;
+            this.PriceMedium = priceMedium;
+            this.PriceBig = priceBig;
+        }
+
+        // Funkcja do dodawania składników
+        public void AddIngredient(string ingredient)
+        {
+            Ingredients.Add(ingredient);
+        }
+
+        // Funkcja do usuwania składników
+        public void RemoveIngredient(string ingredient)
+        {
+            Ingredients.Remove(ingredient);
+        }
+
+        // Funkcja do wyświetlania informacji o pizzy
+        public void ShowInfo()
+        {
+            Console.WriteLine($"Pizza: {name}");
+            Console.WriteLine("Składniki:");
+            foreach (var ingredient in Ingredients)
+            {
+                Console.WriteLine($"- {ingredient}");
+            }
+            Console.WriteLine($"Cena (mała): {priceSmall:C}");
+            Console.WriteLine($"Cena (średnia): {priceMedium:C}");
+            Console.WriteLine($"Cena (duża): {priceBig:C}");
+        }
+    }
+}
