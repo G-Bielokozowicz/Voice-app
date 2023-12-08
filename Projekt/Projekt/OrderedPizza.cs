5d14ae848376713126a3ac5d978a853b08adeefc
Grzegorz Biełokozowicz <grzegorz.bielokozowicz@student.wat.edu.pl>
2023-12-08 13:11:26 +0100

    Added pizz size selection dialog and ordered pizza class

---
+++ Projekt/Projekt/OrderedPizza.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekt
+{
+    public class OrderedPizza
+    {
+        private string name;
+        private List<string> ingredients;
+        private double price;
+        private string size;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public List<string> Ingredients
+        {
+            get { return ingredients; }
+            set { ingredients = value; }
+        }
+
+        public double Price
+        {
+            get { return price; }
+            set { price = value; }
+        }
+
+        public string Size
+        {
+            get { return size; }
+            set { size = value; }
+        }
+
+        public OrderedPizza(string name, List<string> ingredients, double price, string size)
+        {
+            this.Name = name;
+            this.Ingredients = ingredients;
+            this.price = price;
+            this.size = size;
+        }
+
+        // Funkcja do dodawania składników
+        public void AddIngredient(string ingredient)
+        {
+            Ingredients.Add(ingredient);
+        }
+
+        // Funkcja do usuwania składników
+        public void RemoveIngredient(string ingredient)
+        {
+            Ingredients.Remove(ingredient);
+        }
+
+        // Funkcja do wyświetlania informacji o pizzy
+        public void ShowInfo()
+        {
+            Console.WriteLine($"Pizza: {name}");
+            Console.WriteLine("Składniki:");
+            foreach (var ingredient in Ingredients)
+            {
+                Console.WriteLine($"- {ingredient}");
+            }
+            Console.WriteLine($"Cena: {price:C}");
+            Console.WriteLine($"Rozmiar: {size}");
+        }
+    }
+}
---
+++ Projekt/Projekt/PizzaSizeSelectionDialog.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Projekt
+{
+    /// <summary>
+    /// Logika interakcji dla klasy PizzaSizeSelectionDialog.xaml
+    /// </summary>
+    public partial class PizzaSizeSelectionDialog : Window
+    {
+        public string SelectedSize { get; private set; }
+
+        public PizzaSizeSelectionDialog(List<string> pizzaSizes)
+        {
+            InitializeComponent();
+            InitializeComboBox(pizzaSizes);
+        }
+
+        private void InitializeComboBox(List<string> pizzaSizes)
+        {
+            cmbPizzaSize.ItemsSource = pizzaSizes;
+            cmbPizzaSize.SelectedIndex = 0; // Domyślnie wybierz pierwszy rozmiar
+        }
+
+        private void BtnOk_Click(object sender, RoutedEventArgs e)
+        {
+            // Zapisz wybrany rozmiar i zamknij okno
+            SelectedSize = cmbPizzaSize.SelectedItem as string;
+            DialogResult = true;
+            Close();
+        }
+
+        private void BtnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            // Anuluj i zamknij okno
+            DialogResult = false;
+            Close();
+        }
+    }
+}
